d839ed0149fdccb533a6aa517c1bb14cffd35289
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2021-01-27 19:50:07 +0000

    Create encodeDataTo.cpp

---
+++ GD/1.0/classes/gameManager/encodeDataTo.cpp
@@ -0,0 +1,46 @@
+#include <string>
+
+struct scoreKeeper
+{
+    std::string id;
+    int value;
+};
+
+struct valueKeeper
+{
+    std::string id;
+    std::string value;
+};
+
+struct reportedAchievements
+{
+    std::string achievementID;
+    int percentage;
+};
+
+struct gameManager
+{
+    scoreKeeper **scoreKeeper;
+    valueKeeper **valueKeeper;
+    reportedAchievements **reportedAchievements;
+    bool gameCenterEnabled;
+    bool firstSetup;
+    bool showedFirstTutorial;
+    bool musicEnabled;
+    bool fxEnabled;
+    std::string playerUDID;
+    std::string playerName;
+    int playerFrame;
+    int playerColour;
+    int playerColour2;
+    /*
+    GLM and GS Value to be added
+    */
+    bool autoCheckpoints;
+    bool showSongMarkers;
+    bool showBPMMarkers;
+    bool clickedGarage;
+    bool clickedEditor;
+    bool clickedName;
+    bool showedEditorGuide;
+};
