f3f8454157e48e297d1ccdf83ce5745f78b06e66
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2021-01-31 20:39:12 +0000

    Update encodeWithCoder.cpp

--- GD/2.2/classes/songInfoObject/encodeWithCoder.cpp
+++ GD/2.2/classes/songInfoObject/encodeWithCoder.cpp
@@ -1,14 +1,14 @@
 #include <DS_Dictionary>
 
-void encodeWithCoder(SongInfoObject *this, DS_Dictionary *DS_Dictionary)
+void SongInfoObject::encodeWithCoder(SongInfoObject *this, DS_Dictionary *DS_Dictionary)
 {
     GameManager = GameManager::sharedState(this)
         DS_Dictionary->setIntegerForKey("kCEK", 6);
-    DS_Dictionary->setIntegerForKey('1', this->songID);
-    DS_Dictionary->setStringForKey('2', &this->songName);
-    DS_Dictionary->setStringForKey('4', &this->artistName);
-    DS_Dictionary->setIntegerForKey('3', this->artistID);
-    DS_Dictionary->setFloatForKey('5', this->downloadSize);
+    DS_Dictionary->setIntegerForKey('1', songID);
+    DS_Dictionary->setStringForKey('2', songName);
+    DS_Dictionary->setStringForKey('4', artistName);
+    DS_Dictionary->setIntegerForKey('3', artistID);
+    DS_Dictionary->setFloatForKey('5', downloadSize);
     if (GameManager == 1)
     {
         if (this->songYT != 0)
@@ -17,9 +17,9 @@ void encodeWithCoder(SongInfoObject *this, DS_Dictionary *DS_Dictionary)
         }
         if (this->artistYT != 0)
         {
-            DS_Dictionary->setStringForKey('7', &this->artistYT);
+            DS_Dictionary->setStringForKey('7', artistYT);
         }
-        DS_Dictionary->setStringForKey("10", &this->downloadLink);
+        DS_Dictionary->setStringForKey("10", downloadLink);
     }
-    DS_Dictionary->setIntegerForKey('9', this->songPriority);
+    DS_Dictionary->setIntegerForKey('9', songPriority);
 }
