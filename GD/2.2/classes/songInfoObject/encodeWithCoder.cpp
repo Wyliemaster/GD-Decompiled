5fc9d56b4033753b374b3f0b9304c6fddc6eb7d9
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2021-01-31 20:19:25 +0000

    Update encodeWithCoder.cpp

--- GD/2.2/classes/songInfoObject/encodeWithCoder.cpp
+++ GD/2.2/classes/songInfoObject/encodeWithCoder.cpp
@@ -2,7 +2,7 @@
 
 int __fastcall encodeWithCoder(SongInfoObject *this, DS_Dictionary *DS_DictionaryPath)
 {
-    GameManagerExists = GameManager::sharedState(this)
+    GameManager = GameManager::sharedState(this)
         DS_Dictionary::setIntegerForKey(DS_DictionaryPath, "kCEK", 6);
     DS_Dictionary::setIntegerForKey(DS_DictionaryPath, '1', this->songID);
     DS_Dictionary::setStringForKey(DS_DictionaryPath, '2', &this->songName);
