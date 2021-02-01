b321062d9ffd885a7b0667770d68f6772d984944
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2021-02-01 21:40:09 +0000

    renaming variables

--- GD/1.0/classes/gameLevelManager/encodeDataTo.cpp
+++ GD/1.0/classes/gameLevelManager/encodeDataTo.cpp
@@ -1,8 +1,8 @@
 #include <DS_Dictionary>
 void GameLevelManager::encodeDataTo(DS_Dictionary *DS_Dictionary)
 {
-    DS_Dictionary->setDictForKey("GLM_01", GLM01_);  //Official Levels
-    DS_Dictionary->setArrayForKey("GLM_02", GLM02_); //Created Levels
-    DS_Dictionary->setDictForKey("GLM_03", GLM03_);  //Online Levels
-    DS_Dictionary->setDictForKey("GLM_04", GLM04_);  //ratings given to levels
+    DS_Dictionary->setDictForKey("GLM_01", officialLevels_);     //Official Levels
+    DS_Dictionary->setArrayForKey("GLM_02", localLevels_);       //Created Levels
+    DS_Dictionary->setDictForKey("GLM_03", onlineLevels_);       //Online Levels
+    DS_Dictionary->setDictForKey("GLM_04", onlineLevelRatings_); //ratings given to levels
 }
