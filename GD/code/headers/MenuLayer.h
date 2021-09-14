fa40f1a519668b4b74142a12fa4d7bfbc7f47a65
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2021-09-14 20:11:35 +0100

    name correction

--- GD/code/headers/MenuLayer.h
+++ GD/code/headers/MenuLayer.h
@@ -6,7 +6,7 @@ class CCMenuItemSpriteExtra;
 
 class MenuLayer : public cocos2d::CCLayer, FLAlertLayerProtocol, GooglePlayDelegate
 {
-	cocos2d::CCSpriteFrameCache* m_pGooglePlayBtn;
+	cocos2d::CCSpriteFrameCache* m_pGooglePlaySprite;
 	cocos2d::CCSprite* m_pViewProfileText;
 	cocos2d::CCLabelBMFont* m_pPlayerNameLabel;
 	CCMenuItemSpriteExtra* m_pProfileBtn;
