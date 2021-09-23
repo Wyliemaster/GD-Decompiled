fe661cf92907ba96a6cac1ba36ee7eb03c05ce62
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2021-09-23 15:03:31 +0100

    Create SetGroupIDLayer.h

---
+++ GD/code/headers/SetGroupIDLayer.h
@@ -0,0 +1,27 @@
+#include "includes.h"
+
+class SetGroupIDLayer : public FLAlertLayer, TextInputDelegate
+{
+	GameObject* m_pObj;
+	cocos2d::CCArray* m_pObjects;
+	cocos2d::CCArray* m_pZLayerArray;
+	cocos2d::CCArray* m_pGroupArray;
+	cocos2d::CCLabelBMFont* m_pEditorLayerLabel;
+	cocos2d::CCLabelBMFont* m_pEditorLayer2Label;
+	cocos2d::CCLabelBMFont* m_pZOrderLabel;
+	CCTextInputNode* m_pGroupIDInput;
+	int m_nGroupID;
+	int m_nEditorLayer;
+	int m_nEditorLayer2;
+	int m_nZOrder;
+	int m_nZLayer;
+	bool m_bExecutingFunction;
+	bool m_bRemovedGroup;
+	bool m_bHighDetail;
+	bool m_bDontFade;
+	bool m_bDontEnter;
+	int m_nTag;
+	bool m_bGroupParent;
+	bool m_bHasEditedGroups;
+	int m_nNextFreeGroup;
+};
