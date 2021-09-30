58b698002808b07ab42d8188d3f4a5b9f6e37040
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2021-09-30 16:03:12 +0100

    oops

--- GD/code/headers/TableViewCell.h
+++ GD/code/headers/TableViewCell.h
@@ -1,6 +1,24 @@
 #include "includes.h"
 
-class TableViewCell : public BoomListView
+class CCIndexPath;
+class TableViewCell : public cocos2d::CCLayer
 {
-
+	DWORD dwordEC;
+	DWORD dwordF0;
+	CCIndexPath* m_pIndexPath;
+	DWORD dwordF8;
+	DWORD dwordFC;
+	int gap100;
+	int field_134;
+	int field_138;
+	int field_13C;
+	int field_140;
+	int field_144;
+	int field_148;
+	bool field_14C;
+	std::string m_sKey;
+	float m_fTableHeight;
+	float m_fCellHeight;
+	cocos2d::CCLayerColor* m_pColourLayer;
+	cocos2d::CCLayer* m_pLayer;
 };
