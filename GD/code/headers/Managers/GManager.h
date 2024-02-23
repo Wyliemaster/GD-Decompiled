54fcc6e287c3416579ad72f568cbd0dc8a7d5f24
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-02-22 21:14:52 -0600

    Create GManager.h

---
+++ GD/code/headers/Managers/GManager.h
@@ -0,0 +1,36 @@
+#ifndef __GMANAGER_H__
+#define __GMANAGER_H__
+
+#include "../includes.h"
+#include "cocos-ext.h"
+
+/* Special Base Class for all The Game Managers... */
+
+class GManager: cocos2d::CCNode {
+
+    virtual void encodeDataTo(DS_Dictionary *dsdict);
+    virtual bool dataLoaded(DS_Dictionary *dsdict);
+    virtual bool firstLoad();
+
+    std::string getCompressedSaveString();
+    std::string getSaveString();
+    bool init();
+    bool load();
+    bool loadDataFromFile(std::string fileName);
+    bool loadFromCompressedString(std::string compressedStr);
+    bool loadFromString(std::string data);
+
+    void save();
+    bool saveData(DS_Dictionary *dsdict, std::string fileName);
+    void saveGMTo(std::string fileName);
+    void setup();
+    bool tryLoadData(DS_Dictionary* dsdict, std::string fileName);
+
+	std::string m_fileName;
+	bool m_setup;
+	bool m_saved;
+	bool m_quickSave;
+};
+
+
+#endif // __GMANAGER_H__
